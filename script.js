dfb56dbe30c076d6ab3197c8685270c2348649ea
virtualformacion <virtualformacion47@gmail.com>
2026-05-22 00:25:31 +0000

    Actualizar usuarios (edit) desde admin

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ========== USUARIOS AUTORIZADOS ==========
 const USERS = [
     { username: "usurio5", password: "221230", expiresAt: new Date("2027-12-12"), emails: ["correot6@hud.com"] },
-    { username: "usurio", password: "221230", expiresAt: new Date("2027-12-12"), emails: ["correoi5@fioler.com"] }
+    { username: "usurio", password: "221230", expiresAt: new Date("2027-12-12"), emails: ["corredddoi5@fioler.com"] }
 ];
 
 const MAX_ATTEMPTS = 1000000;
