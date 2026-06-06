04ad8aa3c740f93f846602148868e3951172a5b1
virtualformacion <virtualformacion47@gmail.com>
2026-06-06 23:42:26 +0000

    Actualizar usuarios (create) desde admin

--- script.js
+++ script.js
@@ -38,7 +38,8 @@ const USERS = [
     { username: "clienty691", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "client500a", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "dernt6663", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
-    { username: "frenhy677", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] }
+    { username: "frenhy677", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
+    { username: "fernc6685", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] }
 ];
 
 const MAX_ATTEMPTS = 1000000;
