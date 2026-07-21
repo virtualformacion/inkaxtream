4a404aa13fd8a6670c931c7ebf212b373d314d93
virtualformacion <virtualformacion47@gmail.com>
2026-07-21 15:55:55 -0500

    getLastEmail.js

--- netlify/functions/getLastEmail.js
+++ netlify/functions/getLastEmail.js
@@ -49,6 +49,7 @@ exports.handler = async (event) => {
       "Netflix: Tu código de inicio de sesión",
       "Urgente: Tu código de un solo uso de HBO Max",
       "Your one-time passcode for Disney+",
+      "¿Vas a actualizar tu Hogar de Disney+?",
       "Votre code d'accès à usage unique pour Disney+",
       "Votre code d'accès à usage unique pour Disney+",
       "Tu código de acceso único para Disney+" // Asunto específico de Disney+
