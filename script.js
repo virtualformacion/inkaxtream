8f933a372624a4d9100dba08b801fc4a9bc35fcf
virtualformacion <virtualformacion47@gmail.com>
2026-07-30 04:32:41 +0000

    Actualizar usuarios (delete) desde admin

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ const USERS = [
     { username: "ayner577", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "frenkk567", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "client5998", password: "221240", expiresAt: new Date("2027-12-12"), emails: [] },
-    { username: "winxtream4", password: "221240", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "client1002", password: "221240", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "client576", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
     { username: "t196", password: "221230", expiresAt: new Date("2027-12-12"), emails: [] },
